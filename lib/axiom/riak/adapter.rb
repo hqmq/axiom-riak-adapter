bf9bcd3d33dfa0d08b7e91414ed777839800d417
Michael Ries <michael@riesd.com>
2013-11-12 22:01:24 -0700

    starting riak adapter

---
+++ lib/axiom/riak/adapter.rb
@@ -0,0 +1,9 @@
+require "axiom/riak/adapter/version"
+
+module Axiom
+  module Riak
+    module Adapter
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/axiom/riak/adapter/version.rb
@@ -0,0 +1,7 @@
+module Axiom
+  module Riak
+    module Adapter
+      VERSION = "0.0.1"
+    end
+  end
+end
